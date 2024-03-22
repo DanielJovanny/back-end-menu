7ed153e9a097e9921c4aa2fe0551882970ca5797
Daniel Puc <skar.dpm@gmail.com>
2024-03-22 18:38:19 -0500

    feat:funciones de get, post, getOne y delete

--- src/foods/food.controller.ts
+++ src/foods/food.controller.ts
@@ -1,19 +1,42 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
 import { FoodService } from './food.service';
 import { CreateFoodDto } from './dto/create-food.dto';
 import { Food } from './food.entity';
 
 @Controller('food')
 export class FoodController {
-  //Instancia del servicio FoodServoce
+  //Instancia del servicio FoodService
   constructor(private readonly serviceFood: FoodService) {}
 
-  @Post()
+  @Post('save')
+  @HttpCode(201)
   create(@Body() createFoodDto: CreateFoodDto): Promise<Food> {
     return this.serviceFood.create(createFoodDto);
   }
-  @Get()
+  @Get('all')
+  @HttpCode(200)
   findAll(): Promise<Food[]> {
     return this.serviceFood.findAll();
+  }
+
+  @Get('find/:id')
+  @HttpCode(200)
+  findOne(@Param('id', ParseIntPipe) id: number): Promise<Food> {
+    return this.serviceFood.findOne(id);
+  }
+
+  @Delete('delete/:id')
+  @HttpCode(200)
+  delete(@Param('id') id: string): Promise<void> {
+    return this.serviceFood.delete(id);
   }
 }
